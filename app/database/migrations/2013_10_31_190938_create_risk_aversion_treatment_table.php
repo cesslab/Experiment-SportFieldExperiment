79d3b2cc2c5164dd648f2a3bebaa0094731f0a1a
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-05 20:01:11 -0500

    Changed the id type from string to increments, and added the high_prize and gamble_probability columns.

--- app/database/migrations/2013_10_31_190938_create_risk_aversion_treatment_table.php
+++ app/database/migrations/2013_10_31_190938_create_risk_aversion_treatment_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use SportExperiment\Repository\Eloquent\Treatment\RiskAversion;
+use Illuminate\Support\Facades\Schema;
 
 class CreateRiskAversionTreatmentTable extends Migration {
 
@@ -14,11 +15,12 @@ class CreateRiskAversionTreatmentTable extends Migration {
 	{
 		Schema::create(RiskAversion::$TABLE_KEY, function($table)
 		{
-			$table->string(RiskAversion::$ID_KEY)->unique();
+            $table->increments(RiskAversion::$ID_KEY);
 			$table->integer(RiskAversion::$SESSION_ID_KEY)->unsigned();
             $table->integer(RiskAversion::$LOW_PRIZE_KEY);
             $table->integer(RiskAversion::$MID_PRIZE_KEY);
-            $table->integer(RiskAversion::$LOW_PRIZE_KEY);
+            $table->integer(RiskAversion::$HIGH_PRIZE_KEY);
+            $table->integer(RiskAversion::$GAMBLE_PROBABILITY_KEY);
 		});
 	}
 
