c0c04e9a8f5cb88897cc58b6d0b47c0c86cc5dc0
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-05 20:14:14 -0500

    Added the role parameters, accessors, and mutators.

--- app/src/SportExperiment/Repository/Eloquent/Role.php
+++ app/src/SportExperiment/Repository/Eloquent/Role.php
@@ -4,5 +4,28 @@ class Role
 {
     public static $RESEARCHER = 1;
     public static $SUBJECT = 2;
+
+    private $role;
+
+    public function __construct($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
 }
 
