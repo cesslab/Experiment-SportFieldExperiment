d0346b7d243e98c4577902036c330ec13575e2fe
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-05 20:02:11 -0500

    set all columns, except the id and timestamp columns, to nullable.

--- app/database/migrations/2013_11_01_123212_create_subjects_table.php
+++ app/database/migrations/2013_11_01_123212_create_subjects_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use SportExperiment\Repository\Eloquent\Subject;
+use Illuminate\Support\Facades\Schema;
 
 class CreateSubjectsTable extends Migration {
 
@@ -15,15 +16,14 @@ class CreateSubjectsTable extends Migration {
         Schema::create(Subject::$TABLE_KEY, function($table){
             $table->increments(Subject::$ID_KEY);
             $table->integer(Subject::$SESSION_ID_KEY)->unsigned();
-            $table->string(Subject::$USER_ID_KEY);
-            $table->string(Subject::$FIRST_NAME_KEY);
-            $table->string(Subject::$LAST_NAME_KEY);
-            $table->string(Subject::$PROFESSION_KEY);
-            $table->string(Subject::$EDUCATION_KEY);
-            $table->string(Subject::$GENDER_KEY);
-            $table->integer(Subject::$AGE_KEY)->unsigned();
-            $table->string(Subject::$ETHNICITY_KEY);
-            $table->boolean(Subject::$ACTIVE_KEY)->default(false);
+            $table->string(Subject::$USER_ID_KEY)->nullable();
+            $table->string(Subject::$FIRST_NAME_KEY)->nullable();
+            $table->string(Subject::$LAST_NAME_KEY)->nullable();
+            $table->string(Subject::$PROFESSION_KEY)->nullable();
+            $table->string(Subject::$EDUCATION_KEY)->nullable();
+            $table->string(Subject::$GENDER_KEY)->nullable();
+            $table->integer(Subject::$AGE_KEY)->nullable();
+            $table->string(Subject::$ETHNICITY_KEY)->nullable();
             $table->timestamps();
         });
 	}
