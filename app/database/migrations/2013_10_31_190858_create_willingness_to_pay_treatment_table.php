6a05b28ea01100858dbf5bf4112a1b2b1379c945
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-05 20:00:09 -0500

    Changed the id key from type string to increments.

--- app/database/migrations/2013_10_31_190858_create_willingness_to_pay_treatment_table.php
+++ app/database/migrations/2013_10_31_190858_create_willingness_to_pay_treatment_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use SportExperiment\Repository\Eloquent\Treatment\WillingnessPay;
+use Illuminate\Support\Facades\Schema;
 
 class CreateWillingnessToPayTreatmentTable extends Migration {
 
@@ -15,7 +16,7 @@ class CreateWillingnessToPayTreatmentTable extends Migration {
 	{
 		Schema::create(WillingnessPay::$TABLE_KEY, function($table)
 		{
-			$table->string(WillingnessPay::$ID_KEY)->unique();
+            $table->increments(WillingnessPay::$ID_KEY);
 			$table->integer(WillingnessPay::$SESSION_ID_KEY)->unsigned();
             $table->integer(WillingnessPay::$ENDOWMENT_KEY);
 		});
