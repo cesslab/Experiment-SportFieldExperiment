460c324bff81bbe0d3737cfdafca5e2945184ec7
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-06 23:21:10 -0500

    reformatted risk aversion question

--- app/views/site/subject/experiment/treatment/riskAversion.blade.php
+++ app/views/site/subject/experiment/treatment/riskAversion.blade.php
@@ -1,5 +1,13 @@
-<p>There are three money prizes of ${{$low_prize}}, ${{$mid_prize}}, and ${{$high_prize}}.  You have a choice of
-getting {{$mid_prize}} for sure or a gamble: where you can get ${{$high_prize}} with a probability of {{$probability}},
-or ${{$low_prize}} with a probability of 1 - {{$probability}}.</p>
-{{ Form::label('indifference_probability', "Enter the probability that makes you indifferent between $mid_prize, for sure, $high_prize with probability $probability, and $low_prize with probability 1 - $probability.") }}
+<p>
+    There are three money prizes of {{$lowPrize}}, {{$midPrize}}, and {{$highPrize}} dollars.
+    You have a choice of getting {{$midPrize}} dollars for sure or a gamble; where you can get ${{$highPrize}} dollar
+    with a probability of {{$probability}}, or ${{$lowPrize}} dollars with a probability of 1 - {{$probability}}.
+</p>
+
+<p>
+    Enter the probability that makes you indifferent between {{$midPrize}}, for sure, {{$highPrize}} with probability
+    {{$probability}}, and {{$lowPrize}} with probability 1 - {{$probability}}.
+</p>
+
+{{ Form::label('indifference_probability', "Your chosen probability") }}
 {{ Form::text('indifference_probability', '') }}
