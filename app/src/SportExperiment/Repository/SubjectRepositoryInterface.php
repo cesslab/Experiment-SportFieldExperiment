37623d21b13267e683d7dd99cc8d5b535444ebbb
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-06 23:19:17 -0500

    Added function saveSubjectData

--- app/src/SportExperiment/Repository/SubjectRepositoryInterface.php
+++ app/src/SportExperiment/Repository/SubjectRepositoryInterface.php
@@ -1,5 +1,8 @@
 <?php namespace SportExperiment\Repository;
 
+use SportExperiment\Repository\Eloquent\Subject;
+
 interface SubjectRepositoryInterface
 {
+    public function saveSubjectData(Subject $subject, ModelCollection $collection);
 }
