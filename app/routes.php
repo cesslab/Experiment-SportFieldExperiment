5a2679fff2ada892825085d473e772049f019f13
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-06 23:13:34 -0500

    Added pre-game hold state, and experiment routes. Subject filter was also added.

--- app/routes.php
+++ app/routes.php
@@ -10,12 +10,15 @@ use SportExperiment\Controller\Subject\Login as SubjectLogin;
 use SportExperiment\Controller\Subject\Registration;
 use SportExperiment\Filter\AuthorizeSubject;
 use SportExperiment\Controller\Subject\Experiment;
+use SportExperiment\Controller\Subject\PreGameHold;
 
-/*
- * Routes
- */
 Route::get('/', function(){});
 
+/*
+|--------------------------------------------------------------------------
+| Researcher Routes
+|--------------------------------------------------------------------------
+ */
 // Researcher
 Route::get(ResearcherLogin::$URI, ResearcherLogin::getNamespace() . '@getLogin');
 Route::post(ResearcherLogin::$URI, array('before'=>'csrf', 'uses'=>ResearcherLogin::getNamespace() .'@postLogin'));
@@ -29,19 +32,35 @@ Route::get(Session::$URI, array(
 Route::post(Session::$URI, array(
     'before'=>AuthorizeResearcher::$FILTER_NAME, 'uses'=>Session::getNamespace() . '@postSession'));
 
-// Subject
+/*
+|--------------------------------------------------------------------------
+| Subject Routes
+|--------------------------------------------------------------------------
+ */
+// Login
 Route::get(SubjectLogin::$URI, SubjectLogin::getNamespace() . '@getLogin');
 Route::post(SubjectLogin::$URI, array('before'=>'csrf', 'uses'=>SubjectLogin::getNamespace() .'@postLogin'));
 
+// Registration
 Route::get(Registration::$URI, array(
     'before'=>AuthorizeSubject::$FILTER_NAME, 'uses'=>Registration::getNamespace() . '@getRegistration'));
 Route::post(Registration::$URI, array(
     'before'=>AuthorizeSubject::$FILTER_NAME, 'uses'=>Registration::getNamespace() . '@postRegistration'));
 
+// Pre-Game Hold Screen
+Route::get(PreGameHold::$URI, array(
+    'before'=>AuthorizeSubject::$FILTER_NAME, 'uses'=>PreGameHold::getNamespace() . '@getHold'));
+
+// Game
 Route::get(Experiment::$URI, array(
     'before'=>AuthorizeSubject::$FILTER_NAME, 'uses'=>Experiment::getNamespace() . '@getExperiment'));
+Route::post(Experiment::$URI, array(
+    'before'=>AuthorizeSubject::$FILTER_NAME, 'uses'=>Experiment::getNamespace() . '@postExperiment'));
 
 /*
- * Filters
+|--------------------------------------------------------------------------
+| Filters
+|--------------------------------------------------------------------------
  */
 Route::filter(AuthorizeResearcher::$FILTER_NAME, AuthorizeResearcher::getNamespace());
+Route::filter(AuthorizeSubject::$FILTER_NAME, AuthorizeSubject::getNamespace());
