880c75a886af179cf12b154b07dd2ef4de6309fb
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-06 23:14:17 -0500

    Added constructor and postExperiment function

--- app/src/SportExperiment/Controller/Subject/Experiment.php
+++ app/src/SportExperiment/Controller/Subject/Experiment.php
@@ -2,19 +2,48 @@
 
 use Illuminate\Support\Facades\Auth;
 use SportExperiment\Controller\BaseController;
-use SportExperiment\Repository\Eloquent\Treatment\WillingnessPay;
+use SportExperiment\Repository\SubjectRepositoryInterface;
+use SportExperiment\View\Composer\Subject\Experiment as ExperimentComposer;
+use Illuminate\Support\Facades\View;
+use SportExperiment\Repository\Eloquent\Subject\RiskAversion;
+use SportExperiment\Repository\Eloquent\Subject\WillingnessPay;
+use Illuminate\Support\Facades\Input;
+use SportExperiment\Repository\ModelCollection;
+use Illuminate\Support\Facades\Redirect;
 
 class Experiment extends BaseController
 {
     public static $URI = 'subject/experiment';
 
+    private $subjectRepository;
+
+    public function __construct(SubjectRepositoryInterface $subjectRepository)
+    {
+        $this->subjectRepository = $subjectRepository;
+        View::composer(ExperimentComposer::$VIEW_PATH, ExperimentComposer::getNamespace());
+    }
+
     public function getExperiment()
     {
-        $subject = Auth::user()->subject;
-        /* @var $willingnessPay \SportExperiment\Repository\Eloquent\Treatment\WillingnessPay */
-        $willingnessPay = $subject->session->willingnessPay;
+        return View::make(ExperimentComposer::$VIEW_PATH);
+    }
 
-        $riskAversion = $subject->session->riskAversion;
-        var_dump($riskAversion);
+    public function postExperiment()
+    {
+        $modelCollection = new ModelCollection();
+
+        $session = Auth::user()->subject->session;
+
+        if ($session->willingnessPay != null)
+            $modelCollection->addModel(new WillingnessPay(Input::all(), $session->willingnessPay->getEndowment()));
+
+        if ($session->riskAversion != null)
+            $modelCollection->addModel(new RiskAversion(Input::all()));
+
+        if ($modelCollection->validationFails())
+            return Redirect::to(self::$URI)->withInput()->with('errors', $modelCollection->getErrorMessages());
+
+        $this->subjectRepository->saveSubjectData(Auth::user()->subject, $modelCollection);
+        return Redirect::to(self::$URI);
     }
 }
