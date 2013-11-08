83ec72593fdf21a18e33dddb7ad40ad08c43b95b
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-07 20:05:20 -0500

    Created the BaseValidator class

---
+++ app/src/SportExperiment/Validation/BaseValidator.php
@@ -0,0 +1,44 @@
+<?php namespace SportExperiment\Validation;
+
+use Illuminate\Support\Facades\Validator;
+
+class BaseValidator
+{
+    /* @var $validator \Illuminate\Validation\Validator */
+    protected $validator;
+
+    public function __construct($rules)
+    {
+        $this->validator = Validator::make(array(), $rules);
+    }
+
+    public function validationFails()
+    {
+        return $this->validator->fails();
+    }
+
+    public function validationPasses()
+    {
+        return $this->validator->passes();
+    }
+
+    public function getValidator()
+    {
+        return $this->validator;
+    }
+
+    public function getErrorMessages()
+    {
+        return $this->validator->messages();
+    }
+
+    public function setData(array $data)
+    {
+        $this->validator->setData($data);
+    }
+
+    public function getData()
+    {
+        return $this->validator->getData();
+    }
+}
