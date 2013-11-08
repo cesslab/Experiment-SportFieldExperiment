02df49ec7c9fcff76af1c4e6f9f20249e8aa3f8b
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-07 20:02:39 -0500

    added getRoute function

--- app/src/SportExperiment/Controller/Subject/Registration.php
+++ app/src/SportExperiment/Controller/Subject/Registration.php
@@ -1,7 +1,7 @@
 <?php namespace SportExperiment\Controller\Subject;
 
 use SportExperiment\Controller\BaseController;
-use SportExperiment\Repository\Eloquent\Subject\GameState;
+use SportExperiment\Repository\Eloquent\SubjectState;
 use SportExperiment\Repository\SubjectRepositoryInterface;
 use SportExperiment\View\Composer\Subject\Registration as DemographicComposer;
 use \Illuminate\Support\Facades\View;
@@ -12,7 +12,7 @@ use SportExperiment\Controller\Subject\Experiment as ExperimentController;
 
 class Registration extends BaseController
 {
-    public static $URI = 'subject/registration';
+    private static $URI = 'subject/registration';
 
     private $subjectRepository;
 
@@ -32,10 +32,15 @@ class Registration extends BaseController
         $subject = Auth::user()->subject;
         $subject->fill(Input::all());
         if ($subject->validationFails())
-            return Redirect::to(self::$URI)->with('errors', $subject->getErrorMessages());
+            return Redirect::to(self::getRoute())->with('errors', $subject->getErrorMessages());
 
-        $subject->setGameState(new GameState(GameState::$PRE_GAME_HOLD_STATE));
+        $subject->setState(SubjectState::$PRE_GAME_HOLD_STATE);
         $subject->save();
-        return Redirect::to(ExperimentController::$URI);
+        return Redirect::to(ExperimentController::getRoute());
+    }
+
+    public static function getRoute()
+    {
+        return self::$URI;
     }
 }
