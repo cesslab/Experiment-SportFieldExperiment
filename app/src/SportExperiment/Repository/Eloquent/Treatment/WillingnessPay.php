f1457e8fb3799ce7bb38eb70290e1b7aba368e9b
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-08 22:04:26 -0500

    Added the function calculatePayoff.

--- app/src/SportExperiment/Repository/Eloquent/Treatment/WillingnessPay.php
+++ app/src/SportExperiment/Repository/Eloquent/Treatment/WillingnessPay.php
@@ -2,6 +2,8 @@
 
 use SportExperiment\Repository\Eloquent\BaseEloquent;
 use SportExperiment\Repository\Eloquent\Session;
+use SportExperiment\Repository\Eloquent\Subject;
+use SportExperiment\Repository\Eloquent\Subject\WillingnessPay as SubjectWillingnessPay;
 
 class WillingnessPay extends BaseEloquent
 {
@@ -25,11 +27,39 @@ class WillingnessPay extends BaseEloquent
         parent::__construct($attributes);
     }
 
+    /* ---------------------------------------------------------------------
+     * Model Relationships
+     * ---------------------------------------------------------------------*/
     public function session()
     {
         return $this->belongsTo(Session::getNamespace(), self::$SESSION_ID_KEY);
     }
 
+    /* ---------------------------------------------------------------------
+     * Model Routines
+     * ---------------------------------------------------------------------*/
+
+    public function calculatePayoff(Subject $subject)
+    {
+        $willingnessPayEntries = $subject->willingnessPayEntries;
+        /* @var $willingnessPayEntry \SportExperiment\Repository\Eloquent\Subject\WillingnessPay */
+        $willingnessPayEntry = $willingnessPayEntries[rand(0, count($willingnessPayEntries)-1)];
+
+        $endowment = $subject->session->willingnessPay->getEndowment();
+
+        // Generate a random number between 0 and the endowment
+        $randomGoodPrice = lcg_value()*$endowment;
+
+        if ($randomGoodPrice <= $willingnessPayEntry->getWillingnessPay()) {
+            return array(SubjectWillingnessPay::$ITEM_PURCHASED_KEY=>true, SubjectWillingnessPay::$PAYOFF_KEY=>($endowment - $randomGoodPrice));
+        }
+
+        return array(SubjectWillingnessPay::$ITEM_PURCHASED_KEY=>false, SubjectWillingnessPay::$PAYOFF_KEY=>$endowment);
+    }
+
+    /* ---------------------------------------------------------------------
+     * Getters and Setters
+     * ---------------------------------------------------------------------*/
     /**
      * @return mixed
      */
