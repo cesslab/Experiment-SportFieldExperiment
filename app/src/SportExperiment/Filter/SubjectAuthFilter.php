2bd21ff7dbe34133eef33334c879c65af17fe145
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-09 10:58:14 -0500

    Removed unused imports.

--- app/src/SportExperiment/Filter/SubjectAuthFilter.php
+++ app/src/SportExperiment/Filter/SubjectAuthFilter.php
@@ -3,9 +3,7 @@
 use SportExperiment\Repository\Eloquent\Role;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Route;
 use SportExperiment\Controller\Subject\Login;
-use SportExperiment\Router\SubjectRouter;
 
 class SubjectAuthFilter  extends BaseFilter
 {
