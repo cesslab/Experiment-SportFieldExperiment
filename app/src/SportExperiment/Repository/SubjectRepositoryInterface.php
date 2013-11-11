7104596661bd89f749b1ba13075362406c09117c
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-11 15:00:27 -0500

    Added function documentation to saveSubjectData

--- app/src/SportExperiment/Repository/SubjectRepositoryInterface.php
+++ app/src/SportExperiment/Repository/SubjectRepositoryInterface.php
@@ -4,5 +4,10 @@ use SportExperiment\Repository\Eloquent\Subject;
 
 interface SubjectRepositoryInterface
 {
+    /**
+     * @param Subject $subject
+     * @param ModelCollection $collection
+     * @return mixed
+     */
     public function saveSubjectData(Subject $subject, ModelCollection $collection);
 }
