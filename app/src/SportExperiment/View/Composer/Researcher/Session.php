8b756e2a56abdc58d3a8b9ffdd35832c74c50c87
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-11 15:01:53 -0500

    Renamed RiskAversion to RiskAversionTreatment

--- app/src/SportExperiment/View/Composer/Researcher/Session.php
+++ app/src/SportExperiment/View/Composer/Researcher/Session.php
@@ -3,8 +3,8 @@
 use SportExperiment\View\Composer\BaseComposer;
 use SportExperiment\Controller\Researcher\Dashboard as DashboardController;
 use SportExperiment\Repository\Eloquent\Session as SessionModel;
-use SportExperiment\Repository\Eloquent\Treatment\WillingnessPay;
-use SportExperiment\Repository\Eloquent\Treatment\RiskAversion;
+use SportExperiment\Repository\Eloquent\WillingnessPayTreatment;
+use SportExperiment\Repository\Eloquent\RiskAversionTreatment;
 use SportExperiment\Controller\Researcher\Session as SessionController;
 use Illuminate\Support\Facades\URL;
 
@@ -17,11 +17,11 @@ class Session extends BaseComposer
         $view->with('dashboardUrl', URL::to(DashboardController::getRoute()));
         $view->with('postUrl', URL::to(SessionController::getRoute()));
         $view->with('numSubjectsKey', SessionModel::$NUM_SUBJECTS_KEY);
-        $view->with('endowmentKey', WillingnessPay::$ENDOWMENT_KEY);
-        $view->with('lowPrizeKey', RiskAversion::$LOW_PRIZE_KEY);
-        $view->with('midPrizeKey', RiskAversion::$MID_PRIZE_KEY);
-        $view->with('highPrizeKey', RiskAversion::$HIGH_PRIZE_KEY);
-        $view->with('gambleProbKey', RiskAversion::$GAMBLE_PROBABILITY_KEY);
+        $view->with('endowmentKey', WillingnessPayTreatment::$ENDOWMENT_KEY);
+        $view->with('lowPrizeKey', RiskAversionTreatment::$LOW_PRIZE_KEY);
+        $view->with('midPrizeKey', RiskAversionTreatment::$MID_PRIZE_KEY);
+        $view->with('highPrizeKey', RiskAversionTreatment::$HIGH_PRIZE_KEY);
+        $view->with('gambleProbKey', RiskAversionTreatment::$GAMBLE_PROBABILITY_KEY);
     }
 
 } 
