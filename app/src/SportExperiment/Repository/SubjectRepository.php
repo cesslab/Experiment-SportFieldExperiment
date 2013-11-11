90b0b48b168080c59a5ef13002e44d4204ff21de
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-11 15:00:12 -0500

    Added function documentation to saveSubjectData.

--- app/src/SportExperiment/Repository/SubjectRepository.php
+++ app/src/SportExperiment/Repository/SubjectRepository.php
@@ -1,22 +1,27 @@
 <?php namespace SportExperiment\Repository;
 
 use SportExperiment\Repository\Eloquent\Subject;
-use SportExperiment\Repository\Eloquent\Subject\WillingnessPay;
-use SportExperiment\Repository\Eloquent\Subject\RiskAversion;
+use SportExperiment\Repository\Eloquent\WillingnessPayEntry;
+use SportExperiment\Repository\Eloquent\RiskAversionEntry;
 
 class SubjectRepository implements SubjectRepositoryInterface
 {
+    /**
+     * @param Subject $subject
+     * @param ModelCollection $collection
+     * @return null
+     */
     public function saveSubjectData(Subject $subject, ModelCollection $collection)
     {
-        /* @var $willingnessPay \SportExperiment\Repository\Eloquent\Subject\WillingnessPay */
-        $willingnessPay = $collection->getModel(WillingnessPay::getNamespace());
+        /* @var $willingnessPay \SportExperiment\Repository\Eloquent\WillingnessPayEntry */
+        $willingnessPay = $collection->getModel(WillingnessPayEntry::getNamespace());
         if ($willingnessPay != null) {
             $willingnessPay->subject()->associate($subject);
             $willingnessPay->save();
         }
 
-        /* @var $riskAversion \SportExperiment\Repository\Eloquent\Subject\WillingnessPay */
-        $riskAversion = $collection->getModel(RiskAversion::getNamespace());
+        /* @var $riskAversion \SportExperiment\Repository\Eloquent\WillingnessPayEntry */
+        $riskAversion = $collection->getModel(RiskAversionEntry::getNamespace());
         if ($riskAversion != null) {
             $riskAversion->subject()->associate($subject);
             $riskAversion->save();
