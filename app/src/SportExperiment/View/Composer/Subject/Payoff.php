601308fb4e1d77d28b37bbdbb191051c5e7080f1
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-11 15:02:41 -0500

    Added constructor and payoff data.

--- app/src/SportExperiment/View/Composer/Subject/Payoff.php
+++ app/src/SportExperiment/View/Composer/Subject/Payoff.php
@@ -1,13 +1,30 @@
 <?php namespace SportExperiment\View\Composer\Subject;
 
+use Illuminate\Support\Facades\URL;
+use SportExperiment\Repository\Eloquent\Subject;
 use SportExperiment\View\Composer\BaseComposer;
+use SportExperiment\Controller\Subject\Payoff as PayoffController;
 
 class Payoff extends BaseComposer
 {
     public static $VIEW_PATH = 'site.subject.experiment.payoff';
+    private $subject;
+
+
+    public function __construct(Subject $subject)
+    {
+        $this->subject = $subject;
+    }
 
     public function compose($view)
     {
+        $riskAversionEntry = $this->subject->getRiskAversionPayoff();
+        $willingnessPayEntry = $this->subject->getWillingnessPayPayoff();
+        $view->with('riskAversionPayoff', $riskAversionEntry->getPayoff());
+        $view->with('willingnessPayPayoff', $willingnessPayEntry->getPayoff());
+        $view->with('itemPurchased', $willingnessPayEntry->getItemPurchased());
+        $view->with('totalPayoff', $riskAversionEntry->getPayoff()+$willingnessPayEntry->getPayoff());
+        $view->with('postUrl', URL::to(PayoffController::getRoute()));
     }
 
-} 
+}
