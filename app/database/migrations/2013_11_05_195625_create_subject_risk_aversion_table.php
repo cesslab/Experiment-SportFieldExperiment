33c836d57d9d9774a931a73df57d0f514ef8d047
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-11 14:55:04 -0500

    Added a default value of 0.0 for the payoff column.

--- app/database/migrations/2013_11_05_195625_create_subject_risk_aversion_table.php
+++ app/database/migrations/2013_11_05_195625_create_subject_risk_aversion_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 
 use Illuminate\Support\Facades\Schema;
-use SportExperiment\Repository\Eloquent\Subject\RiskAversion as SubjectRiskAversion;
+use SportExperiment\Repository\Eloquent\RiskAversionEntry as SubjectRiskAversion;
 
 class CreateSubjectRiskAversionTable extends Migration {
 
@@ -19,7 +19,7 @@ class CreateSubjectRiskAversionTable extends Migration {
             $table->increments(SubjectRiskAversion::$ID_KEY);
             $table->integer(SubjectRiskAversion::$SUBJECT_ID_KEY)->unsigned();
             $table->double(SubjectRiskAversion::$INDIFFERENCE_PROBABILITY_KEY);
-            $table->double(SubjectRiskAversion::$PAYOFF_KEY);
+            $table->double(SubjectRiskAversion::$PAYOFF_KEY)->default(0.0);
             $table->boolean(SubjectRiskAversion::$SELECTED_FOR_PAYOFF)->default(false);
             $table->timestamps();
         });
