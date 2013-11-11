ec71b18d2fbbbce775ae2c8d033b6c3049fdcac7
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-10 21:21:11 -0500

    Added a controller function and moved the selection, and calculation of subject payoffs the the Subject class.

--- app/src/SportExperiment/Controller/Subject/Payoff.php
+++ app/src/SportExperiment/Controller/Subject/Payoff.php
@@ -2,43 +2,26 @@
 
 use SportExperiment\Controller\BaseController;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Auth;
-use SportExperiment\Repository\Eloquent\Treatment\WillingnessPay;
-use SportExperiment\Repository\Eloquent\Treatment\RiskAversion;
+use SportExperiment\Repository\Eloquent\Subject;
 use SportExperiment\View\Composer\Subject\Payoff as PayoffComposer;
 
 class Payoff extends BaseController
 {
     public static $URI = 'subject/experiment/payoff';
 
+    private $subject;
+
+    function __construct(Subject $subject)
+    {
+        $this->subject = $subject;
+    }
+
+
     public function getPayoff()
     {
-        $payoff = 0;
+        if ( ! $this->subject->isPayoffSet())
+            $this->subject->saveCalculatedPayoffs();
 
-        /* @var $subject \SportExperiment\Repository\Eloquent\Subject */
-        $subject = Auth::user()->subject;
-        if ( ! is_null($subject->getPayoff()))
-            return View::make(PayoffComposer::$VIEW_PATH);
-
-        /* @var $session \SportExperiment\Repository\Eloquent\Session */
-        $session = $subject->session;
-        // Calculate Risk Aversion Payoff
-        if ($session->riskAversion !== null) {
-            $riskAversion = new RiskAversion();
-            $payoff = $riskAversion->calculatePayoff($subject);
-            var_dump($payoff);
-        }
-
-        // Calculate Willingness to Pay Payoff
-        if ($session->willingnessPay !== null) {
-            $willingnessPay = new WillingnessPay();
-            $payoff = $willingnessPay->calculatePayoff($subject);
-            var_dump($payoff);
-        }
-
-        die();
-        $subject->setPayoff($payoff);
-        $subject->save();
         return View::make(PayoffComposer::$VIEW_PATH);
     }
 
