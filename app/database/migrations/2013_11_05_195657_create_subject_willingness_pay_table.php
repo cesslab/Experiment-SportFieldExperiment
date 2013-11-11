dfd7fb2ce7951df644a773adc4394bc0636646c1
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-11 14:55:58 -0500

    Added a default value of 0.0 for the payoff column, and false to the item_purchased column.

--- app/database/migrations/2013_11_05_195657_create_subject_willingness_pay_table.php
+++ app/database/migrations/2013_11_05_195657_create_subject_willingness_pay_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use SportExperiment\Repository\Eloquent\Subject\WillingnessPay as SubjectWillingnessPay;
+use SportExperiment\Repository\Eloquent\WillingnessPayEntry as SubjectWillingnessPay;
 use Illuminate\Support\Facades\Schema;
 
 class CreateSubjectWillingnessPayTable extends Migration {
@@ -18,9 +18,9 @@ class CreateSubjectWillingnessPayTable extends Migration {
             $table->increments(SubjectWillingnessPay::$ID_KEY);
             $table->integer(SubjectWillingnessPay::$SUBJECT_ID_KEY)->unsigned();
             $table->double(SubjectWillingnessPay::$WILLING_PAY_KEY);
-            $table->double(SubjectWillingnessPay::$PAYOFF_KEY);
-            $table->boolean(SubjectWillingnessPay::$ITEM_PURCHASED_KEY);
-            $table->boolean(SubjectWillingnessPay::$SELECTED_FOR_PAYOFF)->default();
+            $table->double(SubjectWillingnessPay::$PAYOFF_KEY)->default(0.0);
+            $table->boolean(SubjectWillingnessPay::$ITEM_PURCHASED_KEY)->default(false);
+            $table->boolean(SubjectWillingnessPay::$SELECTED_FOR_PAYOFF)->default(false);
             $table->timestamps();
         });
 	}
