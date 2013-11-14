a44bf049d945b6355af8932aabdbe16c3afaba9f
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-14 00:01:19 -0500

    Created ultimatum_entry migration

---
+++ app/database/migrations/2013_11_13_133106_create_ultimatum_entry_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use SportExperiment\Model\Eloquent\UltimatumEntry;
+
+class CreateUltimatumEntryTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create(UltimatumEntry::$TABLE_KEY, function($table)
+        {
+            $table->increments(UltimatumEntry::$ID_KEY);
+            $table->integer(UltimatumEntry::$SUBJECT_ID_KEY)->unsigned();
+            $table->double(UltimatumEntry::$AMOUNT_KEY);
+            $table->double(UltimatumEntry::$PAYOFF_KEY)->default(0.0);
+            $table->boolean(UltimatumEntry::$SELECTED_FOR_PAYOFF)->default(false);
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop(UltimatumEntry::$TABLE_KEY);
+	}
+
+}
