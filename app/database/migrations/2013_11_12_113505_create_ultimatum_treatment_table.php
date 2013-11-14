6335fd15324658a44faba85c6fdefe14e49f7cf5
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-14 00:00:46 -0500

    Added ultimatum_treatment migration

---
+++ app/database/migrations/2013_11_12_113505_create_ultimatum_treatment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use SportExperiment\Model\Eloquent\UltimatumTreatment;
+
+class CreateUltimatumTreatmentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create(UltimatumTreatment::$TABLE_KEY, function($table)
+        {
+            $table->increments(UltimatumTreatment::$ID_KEY);
+            $table->integer(UltimatumTreatment::$SESSION_ID_KEY)->unsigned();
+            $table->double(UltimatumTreatment::$TOTAL_AMOUNT_KEY);
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop(UltimatumTreatment::$TABLE_KEY);
+	}
+
+}
