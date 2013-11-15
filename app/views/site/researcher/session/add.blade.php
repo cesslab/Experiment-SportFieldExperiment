7ff40de8881689f778160cd83e4ff4cd616f8ca8
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-15 00:51:03 -0500

    Reformatted add session form.

--- app/views/site/researcher/session/add.blade.php
+++ app/views/site/researcher/session/add.blade.php
@@ -9,23 +9,37 @@
             {{ Form::label($numSubjectsKey, 'Number of Subjects') }}
             {{ Form::text($numSubjectsKey, Input::old($numSubjectsKey)) }}
             <fieldset>
-                <legend>Willingness To Pay</legend>
+                <legend>Task {{$riskAversionTaskId}}: Risk Aversion</legend>
+                <div>
+                    {{ Form::label($lowPrizeKey, 'Low Prize') }}
+                    {{ Form::text($lowPrizeKey, Input::old($lowPrizeKey)) }}
+                </div>
+
+                <div>
+                    {{ Form::label($midPrizeKey, 'Mid Prize') }}
+                    {{ Form::text($midPrizeKey, Input::old($midPrizeKey)) }}
+                </div>
+
+                <div>
+                    {{ Form::label($highPrizeKey, 'High Prize') }}
+                    {{ Form::text($highPrizeKey, Input::old($highPrizeKey)) }}
+                </div>
+
+                <div>
+                    {{ Form::label($gambleProbKey, 'Gamble Probability') }}
+                    {{ Form::text($gambleProbKey, Input::old($gambleProbKey)) }}
+                </div>
+            </fieldset>
+            <fieldset>
+                <legend>Task {{$willingnessPayTaskId}}: Willingness To Pay</legend>
                 {{ Form::label($endowmentKey, 'Endowment') }}
                 {{ Form::text($endowmentKey, Input::old($endowmentKey)) }}
             </fieldset>
             <fieldset>
-                <legend>Risk Aversion</legend>
-                {{ Form::label($lowPrizeKey, 'Low Prize') }}
-                {{ Form::text($lowPrizeKey, Input::old($lowPrizeKey)) }}
+                <legend>Task {{$ultimatumTaskId}}: Ultimatum</legend>
+                {{ Form::label($ultimatumTotalAmountKey, 'Total Amount') }}
+                {{ Form::text($ultimatumTotalAmountKey, Input::old($ultimatumTotalAmountKey)) }}
 
-                {{ Form::label($midPrizeKey, 'Mid Prize') }}
-                {{ Form::text($midPrizeKey, Input::old($midPrizeKey)) }}
-
-                {{ Form::label($highPrizeKey, 'High Prize') }}
-                {{ Form::text($highPrizeKey, Input::old($highPrizeKey)) }}
-
-                {{ Form::label($gambleProbKey, 'Gamble Probability') }}
-                {{ Form::text($gambleProbKey, Input::old($gambleProbKey)) }}
             </fieldset>
             {{ Form::submit('Add New Session') }}
         </fieldset>
