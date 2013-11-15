a22533b5d116921f6a851e835f2586ca67596d0f
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-15 00:52:06 -0500

    Added ultimatum payoff display

--- app/views/site/subject/experiment/payoff.blade.php
+++ app/views/site/subject/experiment/payoff.blade.php
@@ -4,11 +4,14 @@
 <h3>Task {{$riskAversionTaskId}} Payoff: ${{$riskAversionPayoff}}</h3>
 
 <h3>Task {{$willingnessPayTaskId}} Payoff: ${{$willingnessPayPayoff}}</h3>
+
 @if ($itemPurchased)
 <h3>Task {{$willingnessPayTaskId}} Item: Purchased</h3>
 @else
 <span>Task {{$willingnessPayTaskId}} item: Not Purchased</span>
 @endif
+
+<h3>Task {{$ultimatumTaskId}} Payoff: ${{$ultimatumPayoff}}</h3>
 
 <h2>Total Payoff: ${{$totalPayoff}}</h2>
 <h2>Total Items Purchased: {{$itemPurchased}}</h2>
