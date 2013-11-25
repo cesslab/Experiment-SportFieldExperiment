9e48055df16b2f1cdbc947758cd6831dc5cc0ad4
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-24 23:10:19 -0500

    Added the trust treatment to subject experiment template.

--- app/views/site/subject/experiment.blade.php
+++ app/views/site/subject/experiment.blade.php
@@ -13,10 +13,18 @@
     @endif
 
     @if ($displayUltimatum)
-        @if ($isProposer)
+        @if ($isUltimatumProposer)
             @include('site.subject.experiment.treatment.ultimatum_proposer', array('taskId'=>$ultimatumTaskId, 'totalAmount'=>$ultimatumTotalAmount, 'amountKey'=>$amountKey))
         @else
             @include('site.subject.experiment.treatment.ultimatum_receiver', array('taskId'=>$ultimatumTaskId, 'totalAmount'=>$ultimatumTotalAmount, 'amountKey'=>$amountKey))
+        @endif
+    @endif
+
+    @if ($displayTrust)
+        @if ($isTrustProposer)
+            @include('site.subject.experiment.treatment.trust_proposer', array('taskId'=>$trustTaskId, 'proposerAllocationOptions'=>$proposerAllocationOptions, 'numProposerAllocations'=>$numProposerAllocations, 'allocationKey'=>$allocationKey))
+        @else
+            @include('site.subject.experiment.treatment.trust_receiver', array('taskId'=>$trustTaskId, 'receiverAllocationOptions'=>$receiverAllocationOptions, 'numReceiverAllocations'=>$numReceiverAllocations, 'allocationKey'=>$allocationKey))
         @endif
     @endif
 
