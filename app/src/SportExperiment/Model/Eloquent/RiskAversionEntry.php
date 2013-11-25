c658b4741b93908ae84c90cddf2f0caef43761de
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-24 22:59:50 -0500

    Moved common task entry functions and relationships to parent class TaskEntry.

--- app/src/SportExperiment/Model/Eloquent/RiskAversionEntry.php
+++ app/src/SportExperiment/Model/Eloquent/RiskAversionEntry.php
@@ -1,14 +1,10 @@
 <?php namespace SportExperiment\Model\Eloquent;
 
-class RiskAversionEntry extends BaseEloquent
+class RiskAversionEntry extends TaskEntry
 {
     public static $TABLE_KEY = 'risk_aversion_entries';
 
-    public static $ID_KEY = 'id';
-    public static $SUBJECT_ID_KEY = 'subject_id';
     public static $INDIFFERENCE_PROBABILITY_KEY = 'indifference_probability';
-    public static $SELECTED_FOR_PAYOFF = 'selected_for_payoff';
-    public static $PAYOFF_KEY = 'payoff';
 
     protected $rules;
     protected $table;
@@ -30,36 +26,8 @@ class RiskAversionEntry extends BaseEloquent
     }
 
     /* ---------------------------------------------------------------------
-     * Model Relationships
-     * ---------------------------------------------------------------------*/
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function subject()
-    {
-        return $this->belongsTo(Subject::getNamespace(), self::$SUBJECT_ID_KEY);
-    }
-
-    /* ---------------------------------------------------------------------
      * Getters and Setters
      * ---------------------------------------------------------------------*/
-
-    /**
-     * @param $isSelected
-     */
-    public function setSelectedForPayoff($isSelected)
-    {
-        $this->setAttribute(self::$SELECTED_FOR_PAYOFF, $isSelected);
-    }
-
-    /**
-     * @param $payoff
-     */
-    public function setPayoff($payoff)
-    {
-        $this->setAttribute(self::$PAYOFF_KEY, $payoff);
-    }
 
     /**
      * @return mixed
@@ -68,22 +36,4 @@ class RiskAversionEntry extends BaseEloquent
     {
         return $this->getAttribute(self::$INDIFFERENCE_PROBABILITY_KEY);
     }
-
-    /**
-     * @return mixed
-     */
-    public function getPayoff()
-    {
-        return $this->getAttribute(self::$PAYOFF_KEY);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSelectedForPayoff()
-    {
-        return $this->getAttribute(self::$SELECTED_FOR_PAYOFF);
-    }
-
-
-} 
+}
