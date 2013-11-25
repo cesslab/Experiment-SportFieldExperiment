bc0ad0f3a502b74ac7ef7d21cc0ca765ad0c8593
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-24 23:06:03 -0500

    Refactored the saving of treatments into their own respective functions.

--- app/src/SportExperiment/Model/SubjectRepository.php
+++ app/src/SportExperiment/Model/SubjectRepository.php
@@ -1,10 +1,19 @@
 <?php namespace SportExperiment\Model;
 
 use SportExperiment\Model\Eloquent\Subject;
+use SportExperiment\Model\Eloquent\TaskEntry;
+use SportExperiment\Model\Eloquent\TrustProposerEntry;
+use SportExperiment\Model\Eloquent\TrustEntry;
+use SportExperiment\Model\Eloquent\TrustReceiverEntry;
+use SportExperiment\Model\Eloquent\TrustTreatment;
 use SportExperiment\Model\Eloquent\UltimatumEntry;
 use SportExperiment\Model\Eloquent\WillingnessPayEntry;
 use SportExperiment\Model\Eloquent\RiskAversionEntry;
 
+/**
+ * Class SubjectRepository
+ * @package SportExperiment\Model
+ */
 class SubjectRepository implements SubjectRepositoryInterface
 {
     /**
@@ -14,25 +23,60 @@ class SubjectRepository implements SubjectRepositoryInterface
      */
     public function saveSubjectData(Subject $subject, ModelCollection $collection)
     {
-        /* @var $willingnessPayEntry \SportExperiment\Model\Eloquent\WillingnessPayEntry */
-        $willingnessPayEntry = $collection->getModel(WillingnessPayEntry::getNamespace());
-        if ($willingnessPayEntry != null) {
-            $willingnessPayEntry->subject()->associate($subject);
-            $willingnessPayEntry->save();
+        if ($subject->getWillingnessPayTreatment() != null)
+            $this->saveEntry($collection->getModel(WillingnessPayEntry::getNamespace()), $subject);
+
+        if ($subject->getRiskAversionTreatment() != null)
+            $this->saveEntry($collection->getModel(RiskAversionEntry::getNamespace()), $subject);
+
+        if ($subject->getUltimatumTreatment() != null) {
+            $this->saveEntry($collection->getModel(UltimatumEntry::getNamespace()), $subject);
         }
 
-        /* @var $riskAversionEntry \SportExperiment\Model\Eloquent\WillingnessPayEntry */
-        $riskAversionEntry = $collection->getModel(RiskAversionEntry::getNamespace());
-        if ($riskAversionEntry != null) {
-            $riskAversionEntry->subject()->associate($subject);
-            $riskAversionEntry->save();
-        }
+        if ($subject->getTrustTreatment() != null) {
+            $trustEntry = new TrustEntry();
+            $this->saveEntry($trustEntry, $subject);
 
-        /* @var $ultimatumEntry \SportExperiment\Model\Eloquent\UltimatumEntry */
-        $ultimatumEntry = $collection->getModel(UltimatumEntry::getNamespace());
-        if ($ultimatumEntry != null) {
-            $ultimatumEntry->subject()->associate($subject);
-            $ultimatumEntry->save();
+            if ($subject->getTrustGroup()->isProposer())
+               $this->saveTrustProposerEntry($collection->getModel(TrustProposerEntry::getNamespace()), $trustEntry);
+            else
+                $this->saveTrustReceiverEntries($collection->getModel(TrustReceiverEntry::getNamespace()), $trustEntry);
         }
+    }
+
+    /**
+     * @param TrustProposerEntry $proposerEntry
+     * @param TrustEntry $trustEntry
+     */
+    public function saveTrustProposerEntry(TrustProposerEntry $proposerEntry, TrustEntry $trustEntry)
+    {
+        $proposerEntry->trustEntry()->associate($trustEntry);
+        $proposerEntry->save();
+    }
+
+    /**
+     * @param TrustReceiverEntry $receiverEntry
+     * @param TrustEntry $trustEntry
+     */
+    public function saveTrustReceiverEntries(TrustReceiverEntry $receiverEntry, TrustEntry $trustEntry)
+    {
+        $allocations = $receiverEntry->getAllocation();
+        foreach($allocations as $proposerAllocation=>$receiverAllocation) {
+            $receiverEntry = new TrustReceiverEntry();
+            $receiverEntry->setAllocation($receiverAllocation);
+            $receiverEntry->setProposerAllocation($proposerAllocation);
+            $receiverEntry->trustEntry()->associate($trustEntry);
+            $receiverEntry->save();
+        }
+    }
+
+    /**
+     * @param TaskEntry $entry
+     * @param Subject $subject
+     */
+    public function saveEntry(TaskEntry $entry, Subject $subject)
+    {
+        $entry->subject()->associate($subject);
+        $entry->save();
     }
 }
