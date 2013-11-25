050ea370a85b19f804c6e3f624caae36361964a2
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-24 23:04:31 -0500

    Removed the subject(), setPayoff(), getPayoff(), and getSelectedForPayoff function as they are now inherited from TaskEntry.

--- app/src/SportExperiment/Model/Eloquent/WillingnessPayEntry.php
+++ app/src/SportExperiment/Model/Eloquent/WillingnessPayEntry.php
@@ -1,20 +1,20 @@
 <?php namespace SportExperiment\Model\Eloquent;
 
-class WillingnessPayEntry extends BaseEloquent
+class WillingnessPayEntry extends TaskEntry
 {
     public static $TABLE_KEY = 'willingness_pay_entries';
 
-    public static $ID_KEY = 'id';
-    public static $SUBJECT_ID_KEY = 'subject_id';
     public static $WILLING_PAY_KEY = 'willing_pay';
-    public static $PAYOFF_KEY = 'payoff';
     public static $ITEM_PURCHASED_KEY = 'item_purchased';
-    public static $SELECTED_FOR_PAYOFF = 'selected_for_payoff';
 
     protected $rules;
     protected $table;
     protected $fillable;
 
+    /**
+     * @param array $attributes
+     * @param string $endowment
+     */
     public function __construct($attributes = [], $endowment = '100')
     {
         $this->table = self::$TABLE_KEY;
@@ -27,55 +27,38 @@ class WillingnessPayEntry extends BaseEloquent
     }
 
     /* ---------------------------------------------------------------------
-     * Model Relationships
-     * ---------------------------------------------------------------------*/
-
-    public function subject()
-    {
-        return $this->belongsTo(Subject::getNamespace(), self::$SUBJECT_ID_KEY);
-    }
-
-    /* ---------------------------------------------------------------------
      * Getters and Setters
      * ---------------------------------------------------------------------*/
 
+    /**
+     * @param $willingnessPay
+     */
     public function setWillingnessPay($willingnessPay)
     {
         $this->setAttribute(self::$WILLING_PAY_KEY, $willingnessPay);
     }
 
-    public function setPayoff($payoff)
-    {
-        $this->setAttribute(self::$PAYOFF_KEY, $payoff);
-    }
-
+    /**
+     * @param $itemPurchased
+     */
     public function setItemPurchased($itemPurchased)
     {
         $this->setAttribute(self::$ITEM_PURCHASED_KEY, $itemPurchased);
     }
 
-    public function setSelectedForPayoff($isSelected)
-    {
-        $this->setAttribute(self::$SELECTED_FOR_PAYOFF, $isSelected);
-    }
-
+    /**
+     * @return mixed
+     */
     public function getWillingnessPay()
     {
         return $this->getAttribute(self::$WILLING_PAY_KEY);
     }
 
-    public function getPayoff()
-    {
-        return $this->getAttribute(self::$PAYOFF_KEY);
-    }
-
+    /**
+     * @return mixed
+     */
     public function getItemPurchased()
     {
         return $this->getAttribute(self::$ITEM_PURCHASED_KEY);
-    }
-
-    public function getSelectedForPayoff()
-    {
-        return $this->getAttribute(self::$SELECTED_FOR_PAYOFF);
     }
 }
