b72a362c89330acec8cc265b0f48966a7f3855d7
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-27 15:07:04 -0500

    added trust proposer and receiver entry getter functions.

--- app/src/SportExperiment/Model/Eloquent/TrustEntry.php
+++ app/src/SportExperiment/Model/Eloquent/TrustEntry.php
@@ -1,5 +1,6 @@
 <?php namespace SportExperiment\Model\Eloquent;
 
+use Illuminate\Support\Facades\Log;
 
 class TrustEntry extends TaskEntry
 {
@@ -32,10 +33,42 @@ class TrustEntry extends TaskEntry
     }
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
      */
     public function trustProposerEntries()
     {
-        return $this->hasMany(TrustProposerEntry::getNamespace(), TrustProposerEntry::$TRUST_ENTRY_ID_KEY);
+        return $this->hasOne(TrustProposerEntry::getNamespace(), TrustProposerEntry::$TRUST_ENTRY_ID_KEY);
+    }
+
+    /**
+     * @return TrustProposerEntry
+     */
+    public function getProposerEntry()
+    {
+        return $this->trustProposerEntries;
+    }
+
+    /**
+     * @return TrustReceiverEntry[]
+     */
+    public function getReceiverEntries()
+    {
+        return $this->trustReceiverEntries;
+    }
+
+    /**
+     * @param TrustProposerEntry
+     * @param TrustTreatment
+     * @return TrustReceiverEntry
+     */
+    public function getReceiverAllocationEntry(TrustProposerEntry $proposerEntry, TrustTreatment $trustTreatment)
+    {
+        $receiverEntries = $this->getReceiverEntries();
+        $receiverMultiplier = $trustTreatment->getReceiverAllocationMultiplier();
+        foreach ($receiverEntries as $receiverEntry) {
+            if (($proposerEntry->getAllocation() * $receiverMultiplier) == $receiverEntry->getProposerAllocation())
+                return $receiverEntry;
+        }
+        Log::error('Failed to find a receiver entry for the proposers allocation.');
     }
 }
