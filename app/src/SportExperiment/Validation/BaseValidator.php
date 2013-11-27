3bb1b028bfae3f210bd2502974ba6a81b08cf249
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-27 15:10:31 -0500

    Added PHPDoc to functions.

--- app/src/SportExperiment/Validation/BaseValidator.php
+++ app/src/SportExperiment/Validation/BaseValidator.php
@@ -7,41 +7,76 @@ class BaseValidator
     /* @var $validator \Illuminate\Validation\Validator */
     protected $validator;
 
+    /* @var $rules array */
+    protected $rules;
+
+    /**
+     * @param $rules
+     */
     public function __construct($rules)
     {
         $this->validator = Validator::make([], $rules);
     }
 
+    /**
+     * Clears validation rules.
+     */
+    public function clearValidationRules()
+    {
+        $this->rules = [];
+    }
+
+    /**
+     * @return bool
+     */
     public function validationFails()
     {
         return $this->validator->fails();
     }
 
+    /**
+     * @return bool
+     */
     public function validationPasses()
     {
         return $this->validator->passes();
     }
 
+    /**
+     * @return \Illuminate\Validation\Validator
+     */
     public function getValidator()
     {
         return $this->validator;
     }
 
+    /**
+     * @return \Illuminate\Support\MessageBag
+     */
     public function getErrorMessages()
     {
         return $this->validator->messages();
     }
 
+    /**
+     * @param array $data
+     */
     public function setData(array $data)
     {
         $this->validator->setData($data);
     }
 
+    /**
+     * @return array
+     */
     public function getData()
     {
         return $this->validator->getData();
     }
 
+    /**
+     * @return array
+     */
     public function getRules()
     {
         return $this->validator->getRules();
