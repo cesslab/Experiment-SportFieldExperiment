96f73daefe73394e90a5a81da8cde98ebfc289f3
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-27 15:10:22 -0500

    Added PHPDoc to functions.

--- app/src/SportExperiment/Model/ResearcherRepositoryInterface.php
+++ app/src/SportExperiment/Model/ResearcherRepositoryInterface.php
@@ -5,12 +5,32 @@ use SportExperiment\Model\Eloquent\User;
 interface ResearcherRepositoryInterface
 {
 
+    /**
+     * @param User $user
+     * @return bool
+     */
     public function isResearcher(User $user);
 
+    /**
+     * @param ModelCollection $modelCollection
+     * @return mixed
+     */
     public function saveSession(ModelCollection $modelCollection);
 
+    /**
+     * @param $id
+     * @return Eloquent\Session
+     */
+    public function getSession($id);
+
+    /**
+     * @return Eloquent\Session[]
+     */
     public function getSessions();
 
+    /**
+     * @return Eloquent\Subject[]
+     */
     public function getSubjects();
 }
 
