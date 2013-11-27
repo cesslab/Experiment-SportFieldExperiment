9ad2106fd5e0b62d4d084f7dd3255c3e47a26b6a
Anwar A. Ruff <anwarruff@gmail.com>
2013-11-27 15:09:39 -0500

    Removed setter and getter functions that are now in the UltimatumGroup class.

--- app/src/SportExperiment/Model/Eloquent/UltimatumEntry.php
+++ app/src/SportExperiment/Model/Eloquent/UltimatumEntry.php
@@ -5,12 +5,6 @@ class UltimatumEntry extends TaskEntry
     public static $TABLE_KEY = 'ultimatum_entries';
 
     public static $AMOUNT_KEY = 'amount';
-    public static $PARTNER_ID_KEY = 'partner_id';
-    public static $PARTNER_ENTRY_KEY = 'partner_entry_id';
-    public static $PARTNER_AMOUNT_KEY = 'partner_amount';
-
-    private static $MIN_KEY = 2;
-    private static $MAX_KEY = 3;
 
     protected $rules;
     protected $table;
@@ -22,7 +16,7 @@ class UltimatumEntry extends TaskEntry
     public function __construct($attributes = [])
     {
         $this->table = self::$TABLE_KEY;
-        $this->rules = [self::$AMOUNT_KEY=>['required', 'numeric', self::$MIN_KEY=>'min:0', self::$MAX_KEY=>'max:1000']];
+        $this->rules = [self::$AMOUNT_KEY=>['required', 'numeric', 'min:0']];
 
         $this->fillable = [self::$AMOUNT_KEY];
 
@@ -42,35 +36,11 @@ class UltimatumEntry extends TaskEntry
     }
 
     /**
-     * @param $partnerId
-     */
-    public function setPartnerId($partnerId)
-    {
-        $this->setAttribute(self::$PARTNER_ID_KEY, $partnerId);
-    }
-
-    /**
-     * @param $amount
-     */
-    public function setPartnerAmount($amount)
-    {
-        $this->setAttribute(self::$PARTNER_AMOUNT_KEY, $amount);
-    }
-
-    /**
-     * @param $partnerEntryId
-     */
-    public function setPartnerEntryId($partnerEntryId)
-    {
-        $this->setAttribute(self::$PARTNER_ENTRY_KEY, $partnerEntryId);
-    }
-
-    /**
      * Sets the maximum value allowed for the amount value, enforced during validation.
      * @param int $size
      */
     public function setMaxAmountRule($size)
     {
-        $this->rules[self::$AMOUNT_KEY][self::$MAX_KEY] = 'max:' . $size;
+        $this->rules[self::$AMOUNT_KEY][] = 'max:' . $size;
     }
 }
