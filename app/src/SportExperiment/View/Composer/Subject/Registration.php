9f7c7f308448b1fe731fcc87dfbdda2340d2f868
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-02 03:18:54 -0500

    Added registration keys to the registration view composer.

--- app/src/SportExperiment/View/Composer/Subject/Registration.php
+++ app/src/SportExperiment/View/Composer/Subject/Registration.php
@@ -4,6 +4,7 @@
 use SportExperiment\View\Composer\BaseComposer;
 use Illuminate\Support\Facades\URL;
 use SportExperiment\Controller\Subject\Registration as RegistrationController;
+use SportExperiment\Model\Eloquent\Subject;
 
 class Registration extends BaseComposer
 {
@@ -12,5 +13,12 @@ class Registration extends BaseComposer
     public function compose($view)
     {
         $view->with('postUrl', URL::to(RegistrationController::getRoute()));
+        $view->with('firstName', Subject::$FIRST_NAME_KEY);
+        $view->with('lastName', Subject::$LAST_NAME_KEY);
+        $view->with('profession', Subject::$PROFESSION_KEY);
+        $view->with('education', Subject::$EDUCATION_KEY);
+        $view->with('gender', Subject::$GENDER_KEY);
+        $view->with('ethnicity', Subject::$ETHNICITY_KEY);
+        $view->with('age', Subject::$AGE_KEY);
     }
 }
