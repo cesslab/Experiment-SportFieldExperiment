2eedf38fc56678dfc95a66140769820310faa619
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-02 03:29:52 -0500

    New DB seeder

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use SportExperiment\Model\Eloquent\UserRole;
+use SportExperiment\Model\Eloquent\User;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder 
+{
+    public function run()
+    {
+    }
+}
