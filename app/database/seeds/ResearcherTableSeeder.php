f8fdb4126fbccfe50df7d308635a10eeacab7688
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-02 03:34:22 -0500

    seeder not used.

--- app/database/seeds/ResearcherTableSeeder.php
+++ app/database/seeds/ResearcherTableSeeder.php
@@ -7,14 +7,5 @@ class ResearcherTableSeeder
 {
     public function run()
     {
-        DB::table(Researcher::$TABLE_KEY)->delete();
-        Researcher::create(array(
-            Researcher::$ID_KEY=>1,
-            Researcher::$FIRST_NAME_KEY=>'Anwar',
-            Researcher::$LAST_NAME_KEY=>'Ruff',
-            Researcher::$EMAIL_KEY=>'anwar.ruff@nyu.edu',
-            'created_at'=>new DateTime,
-            'updated_at'=>new DateTime
-        ));
     }
 }
