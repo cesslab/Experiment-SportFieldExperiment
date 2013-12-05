fa773588d84e663fa023fc8f6b734698e0ce2504
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-05 17:12:57 -0500

    add seeder

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -9,5 +9,15 @@ class UserTableSeeder extends Seeder
 {
     public function run()
     {
+        DB::table(User::$TABLE_KEY)->delete();
+        User::create(array(
+            User::$ID_KEY=>1,
+            User::$USER_NAME_KEY=>'',
+            User::$PASSWORD_KEY=>Hash::make(''),
+            User::$ROLE_KEY=>  UserRole::$RESEARCHER,
+            User::$ACTIVE_KEY=>true,
+            'created_at'=>new DateTime,
+            'updated_at'=>new DateTime
+        ));
     }
 }
