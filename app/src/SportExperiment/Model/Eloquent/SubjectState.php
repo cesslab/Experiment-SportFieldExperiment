eaffea5998863b7ccb1ad2b5da871bd4e6e1c9e4
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-13 11:48:11 -0500

    Added PHPDoc

--- app/src/SportExperiment/Model/Eloquent/SubjectState.php
+++ app/src/SportExperiment/Model/Eloquent/SubjectState.php
@@ -3,14 +3,18 @@
 class SubjectState
 {
     public static $REGISTRATION = 1;
-    public static $PRE_GAME_HOLD_STATE = 2;
-    public static $GAME_PLAY = 3;
-    public static $PAYOFF = 4;
-    public static $OUTGOING_QUESTIONNAIRE = 5;
-    public static $COMPLETED = 6;
+    public static $PRE_GAME_QUESTIONNAIRE_STATE = 2;
+    public static $PRE_GAME_HOLD_STATE = 3;
+    public static $GAME_PLAY = 4;
+    public static $PAYOFF = 5;
+    public static $OUTGOING_QUESTIONNAIRE = 6;
+    public static $COMPLETED = 7;
 
     private $gameState;
 
+    /**
+     * @param $gameState
+     */
     public function __construct($gameState)
     {
         $this->gameState = $gameState;
@@ -32,33 +36,79 @@ class SubjectState
         return $this->gameState;
     }
 
+    /**
+     * Returns true if the provided state is the Registration State.
+     *
+     * @param $state
+     * @return bool
+     */
     public static function isRegistrationState($state)
     {
         return $state == self::$REGISTRATION;
     }
 
+    /**
+     * Returns true if the provided state is the Pre-Game Hold State.
+     *
+     * @param $state
+     * @return bool
+     */
     public static function isPreGameHoldState($state)
     {
         return $state == self::$PRE_GAME_HOLD_STATE;
     }
 
+    /**
+     * Returns true if the provided state is the Game Play State.
+     *
+     * @param $state
+     * @return bool
+     */
     public static function isGamePlayState($state)
     {
         return $state == self::$GAME_PLAY;
     }
 
+    /**
+     * Returns true if the provided state is the Outgoing Questionnaire State.
+     *
+     * @param $state
+     * @return bool
+     */
     public static function isOutgoingQuestionnaireState($state)
     {
         return $state == self::$OUTGOING_QUESTIONNAIRE;
     }
 
+    /**
+     * Returns true if the provided state is the Payoff State.
+     *
+     * @param $state
+     * @return bool
+     */
     public static function isPayoffState($state)
     {
         return $state == self::$PAYOFF;
     }
 
+    /**
+     * Returns true if the current state is the Completed State.
+     *
+     * @param $state
+     * @return bool
+     */
     public static function isComplete($state)
     {
         return $state == self::$COMPLETED;
+    }
+
+    /**
+     * Returns true if the current state is the Pre-Game Questionnaire State.
+     * @param $state
+     * @return bool
+     */
+    public static function isPreGameQuestionnaireState($state)
+    {
+        return $state == self::$PRE_GAME_QUESTIONNAIRE_STATE;
     }
 }
