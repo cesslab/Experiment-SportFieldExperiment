b83a94aef57c11cb9e4afc9c347d6f881320b447
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-13 11:49:52 -0500

    updated saveSubjectData function signature

--- app/src/SportExperiment/Model/SubjectRepositoryInterface.php
+++ app/src/SportExperiment/Model/SubjectRepositoryInterface.php
@@ -7,7 +7,8 @@ interface SubjectRepositoryInterface
     /**
      * @param Subject $subject
      * @param ModelCollection $collection
+     * @param $nextTreatment
      * @return mixed
      */
-    public function saveSubjectData(Subject $subject, ModelCollection $collection);
+    public function saveSubjectData(Subject $subject, ModelCollection $collection, $nextTreatment);
 }
