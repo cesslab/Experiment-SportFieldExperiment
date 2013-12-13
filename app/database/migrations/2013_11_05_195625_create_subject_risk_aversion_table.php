721a676be9f27f5abf8c85d21fe37c1b07ecb3e0
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-13 11:43:56 -0500

    removed Indifference prob and added game payment

--- app/database/migrations/2013_11_05_195625_create_subject_risk_aversion_table.php
+++ app/database/migrations/2013_11_05_195625_create_subject_risk_aversion_table.php
@@ -18,7 +18,7 @@ class CreateSubjectRiskAversionTable extends Migration {
         {
             $table->increments(SubjectRiskAversion::$ID_KEY);
             $table->integer(SubjectRiskAversion::$SUBJECT_ID_KEY)->unsigned();
-            $table->double(SubjectRiskAversion::$INDIFFERENCE_PROBABILITY_KEY);
+            $table->double(SubjectRiskAversion::$GAMBLE_PAYMENT_KEY);
             $table->double(SubjectRiskAversion::$PAYOFF_KEY)->default(0.0);
             $table->boolean(SubjectRiskAversion::$SELECTED_FOR_PAYOFF)->default(false);
             $table->timestamps();
