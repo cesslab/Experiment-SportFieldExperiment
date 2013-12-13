cb6fdf651248ec412947f7d43c37eb5b5afe1ceb
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-13 11:50:06 -0500

    Added PHPDoc

--- app/src/SportExperiment/Router/SubjectRouter.php
+++ app/src/SportExperiment/Router/SubjectRouter.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Request;
 use SportExperiment\Controller\Researcher\Login;
-use SportExperiment\Controller\Subject\Registration;
+use SportExperiment\Controller\Subject\PreGame\Registration;
 use SportExperiment\Controller\Subject\PreGameHold;
 use SportExperiment\Controller\Subject\Experiment;
 use SportExperiment\Model\Eloquent\Subject;
@@ -12,13 +12,17 @@ use SportExperiment\Controller\Subject\Payoff;
 use SportExperiment\Controller\Subject\Questionnaire;
 use SportExperiment\Controller\Subject\Completed;
 
-class SubjectRouter {
+use SportExperiment\Controller\Subject\PreGame\Questionnaire as PreGameQuestionnaire;
+
+class SubjectRouter
+{
     private $route;
 
     public function __construct()
     {
         $this->route = [
             SubjectState::$REGISTRATION=>Registration::getRoute(),
+            SubjectState::$PRE_GAME_QUESTIONNAIRE_STATE=>PreGameQuestionnaire::getRoute(),
             SubjectState::$PRE_GAME_HOLD_STATE=>PreGameHold::getRoute(),
             SubjectState::$GAME_PLAY=>Experiment::getRoute(),
             SubjectState::$COMPLETED=>Login::getRoute(),
@@ -45,6 +49,8 @@ class SubjectRouter {
         $sessionState = $subject->session->getState();
         $subjectGameState = $subject->getState();
 
+        // Todo: move game play state change to Session->updateSession()
+
         /*
          * The subject's game state is auto transitioned in two cases:
          */
@@ -53,11 +59,13 @@ class SubjectRouter {
             $subject->save();
             return $this->route[SubjectState::$GAME_PLAY];
         }
+
         if (SubjectState::isGamePlayState($subjectGameState) && SessionState::isStoppedState($sessionState)) {
             $subject->setState(SubjectState::$PAYOFF);
             $subject->save();
             return $this->route[SubjectState::$PAYOFF];
         }
+
 
         return $this->route[$subjectGameState];
     }
