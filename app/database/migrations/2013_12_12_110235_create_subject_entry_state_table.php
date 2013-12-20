a2830c067d7e6337f160850ebf7b139446f57e50
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-20 18:23:55 -0500

    Refactored column names

--- app/database/migrations/2013_12_12_110235_create_subject_entry_state_table.php
+++ app/database/migrations/2013_12_12_110235_create_subject_entry_state_table.php
@@ -16,9 +16,10 @@ class CreateSubjectEntryStateTable extends Migration {
         Schema::create(SubjectEntryState::$TABLE_KEY, function($table){
             $table->increments(SubjectEntryState::$ID_KEY);
             $table->integer(SubjectEntryState::$SUBJECT_ID_KEY)->unsigned();
-            $table->integer(SubjectEntryState::$CURRENT_TASK_ID_KEY)->unsigned()->default(0);
-            $table->boolean(SubjectEntryState::$TASKS_COMPLETED_KEY)->default(false);
-            $table->boolean(SubjectEntryState::$QUESTIONS_COMPLETED_KEY)->default(false);
+            $table->integer(SubjectEntryState::$TASK_ID_KEY)->unsigned()->default(0);
+            $table->integer(SubjectEntryState::$ORDER_ID_KEY)->unsigned()->default(0);
+            $table->boolean(SubjectEntryState::$TASK_ENTRY_STATE_KEY)->default(false);
+            $table->boolean(SubjectEntryState::$QUESTION_ENTRY_STATE_KEY)->default(false);
         });
 	}
 
