40a57dc8bdd8f2d95663ec0a3d6c908e5f94430e
Anwar A. Ruff <anwarruff@gmail.com>
2013-12-26 14:01:39 -0500

    Added dynamic loading of the subjects table

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -2,5 +2,11 @@ $(document).ready(function()
     {
         $("#sessionTable").tablesorter();
         $("#subjectTable").tablesorter();
+        refreshTable();
+        function refreshTable(){
+            $('#subjectTable').load('subject_table', function(){
+                setTimeout(refreshTable, 15000);
+            });
+        }
     }
 );
